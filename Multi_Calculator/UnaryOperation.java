84323495075efb17e8b2950bb352bac4522ed570
Frisk9673 <ktq2390301@stu.o-hara.ac.jp>
2024-02-05 12:04:22 +0900

    Commit

--- Multi_Calculator/UnaryOperation.java
+++ Multi_Calculator/UnaryOperation.java
@@ -1,5 +1,5 @@
 package Multi_Calculator;
-//一項演算の基底クラス
+//単項演算の基底クラス
 abstract class UnaryOperation implements Operation {
     //単項演算を実行するメソッド(num2はダミーパラメータ扱いとする)
     public abstract double operate(double num1, double num2);
